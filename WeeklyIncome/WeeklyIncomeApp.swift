da1962e23e2fe2f30c32f24223590025b422068a
Jun <wnsgur4092@gmail.com>
2022-09-24 21:42:28 +1000

    Initial Commit

---
+++ WeeklyIncome/WeeklyIncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyIncomeApp.swift
+//  WeeklyIncome
+//
+//  Created by JunHyuk Lim on 24/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyIncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
